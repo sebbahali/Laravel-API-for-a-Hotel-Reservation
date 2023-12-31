1a5db2d143c3691faf64792018d785bf6386c40e
sb ali <125546478+sebbahali@users.noreply.github.com>
2023-12-31 07:55:33 -0800

    Update HotelPolicy.php

--- app/Policies/HotelPolicy.php
+++ app/Policies/HotelPolicy.php
@@ -9,29 +9,38 @@ use Illuminate\Auth\Access\Response;
 class HotelPolicy
 {
    
-    public function create(User $user): bool
+    public function create(User $user): Response
     {
       
-     return $user->role->name == "Admin" || $user->role->name == 'Owner';
+        return $this->getResponse($user->role->name == "Admin" || $user->role->name == 'Owner');
+   
 
     }
 
     /**
      * Determine whether the user can update the model.
      */
-    public function update(User $user , Hotel $hotel): bool
+    public function update(User $user , Hotel $hotel): Response
     {
-        return ($user->role->name == 'Admin' || $user->role->name == 'Owner') &&  $user->id === $hotel->user_id ;
+        return $this->getResponse(($user->role->name == 'Admin' || $user->role->name == 'Owner') && $user->id === $hotel->user_id);
+
     }
 
     /**
      * Determine whether the user can delete the model.
      */
-    public function delete(User $user, Hotel $hotel): bool
+    public function delete(User $user, Hotel $hotel) : Response
     {
-        return ($user->role->name == 'Admin' || $user->role->name == 'Owner') &&  $user->id === $hotel->user_id ;
+        return $this->getResponse(($user->role->name == 'Admin' || $user->role->name == 'Owner') &&  $user->id === $hotel->user_id );
+
     }
-
-
   
+    
+    private function getResponse(bool $hasPermission): Response
+    {
+        return $hasPermission
+            ? Response::allow()
+            : Response::deny('You do not have permission');
+    }
+    
 }
