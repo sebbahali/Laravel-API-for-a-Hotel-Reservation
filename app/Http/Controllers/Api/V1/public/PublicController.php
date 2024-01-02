04bb22a0fb7138c42018895c548e6bdd4094aa7b
sb ali <125546478+sebbahali@users.noreply.github.com>
2024-01-02 14:30:21 -0800

    Update PublicController.php

--- app/Http/Controllers/Api/V1/public/PublicController.php
+++ app/Http/Controllers/Api/V1/public/PublicController.php
@@ -11,31 +11,33 @@ class PublicController extends Controller
 {
    public function __invoke(Request $request)
     {
-    $hotels = Hotel::with('rooms')
+
+        $hotels = Hotel::query()
        
         ->when($request->address, function ($query) use ($request) {
 
             $query->where('address', $request->address);
 
-    })->when ($request->adults && $request->children , function($query) use ($request){
+        })->withwherehas('rooms', function($query) use ($request) {
 
-        $query->wherehas('rooms', function($query) use ($request) {
-        $query->where('adults', '>=' , $request->adults)
-        ->where('children', '>=' ,$request->children);
-        }); 
-           
-    })->when ($request->from_price && $request->to_price , function($query) use ($request){
-           
-        $query->wherehas('rooms', function($query) use ($request) {
-        $query->where('price', '>=' , $request->from_price)
-        ->where('price', '<=' , $request->to_price);
-    
-        });
-    ->get(); 
-        
+            $query->when ($request->adults && $request->children , function($query) use ($request){
+
+                $query->where('adults', '>=' , $request->adults)
+                ->where('children', '>=' ,$request->children);
+            }) 
+            ->when ($request->from_price && $request->to_price , function($query) use ($request){
+
+                $query->whereBetween('price',[$request->from_price,$request->to_price ]);
+                
+            });
+        })
+        ->get(); 
+
+
+
         return HotelResource::collection($hotels);
 
-    }
+}
    
 }
 
