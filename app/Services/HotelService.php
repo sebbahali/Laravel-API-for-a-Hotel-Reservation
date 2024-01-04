2587db515ba33d91d327ffce02f9f5705b6a4c5b
sb ali <125546478+sebbahali@users.noreply.github.com>
2024-01-04 15:30:01 -0800

    Update HotelService.php

--- app/Services/HotelService.php
+++ app/Services/HotelService.php
@@ -1,90 +1,100 @@
 <?php
 namespace App\Services;
 
-use App\Models\Hotel;
+use App\Models\Room;
 use App\Utils\ImageUpload;
 use Illuminate\Support\Facades\Storage;
+use Exception;
 
-class HotelService  
+
+   class RoomService
+   
 {
+   public function getAll(){
 
-  public function getAll()
-  {
-
-   return Hotel::with('rooms')->get();
-
-  }
-    public function Store($request)
-     {
-      $data = $request->validated();
-      
-      try{
-
-        if ($request->hasfile('image')) {
-
-          $path=ImageUpload::uploadSingleImage($request);
-          
-          $data['image'] = $path;
-
-        }
-      
-         $hotel = Hotel::create($data);
-
-          return $hotel;
-         
-           
-      } catch(\Exception $e)
-      {
-        return response()->json([
-          'message'=>$e
-       ]);
-
-      }
-        
-    }
-    public function getByid($hotel)
-     {
-
-      return $hotel->load('rooms');
-   }
-
-
-    public function Update($request , $hotel)
-    {
-      $data = $request->validated();
-
-      try {
-        if ($request->hasfile('image')) {
-
-          $path=ImageUpload::uploadSingleImage($request);
-          
-          $data['image'] = $path;
-
-        }
-      
-                     
-          $hotel->update($data);
-          
-          return $hotel;
-          
-      }catch(\Exception $e)
-      {
-
-        return response()->json([
-          'message'=>$e
-       ]);
-
-        
-      }
-      
-
-   }
-
-     public function Delete($hotel)
-     {        
-      (Storage::exists($hotel->image)) ? Storage::delete($hotel->image) :  \Log::error("Error deleting file: $hotel->image)"); 
+      return Room::with('hotel')->get();
+       
     
-      $hotel->delete();
+       }
+   
+     public function  Store( $request )
+
+     {
+    
+        $data = $request->validated();
+
+        try{
+
+         if ($request->hasFile('images')) {
+  
+            $files = ImageUpload::uploadMultipleImages($request);
+
+            $data['images'] = json_encode($files);
+            }
+      
+         }
+           catch (\Exception $e)
+           { 
+
+            return response()->json([
+               'message'=>$e
+            ]);
+
+           }
+           
+           return Room::create($data);
+                    
+           
+    }
+    public function getByid($Room){
+  
+  
+      return $Room->load('hotel')->get();
+   }
+   
+    public function Update($request ,$Room )
+
+    {
+   
+       $data = $request->validated();
+
+       try{
+
+        if ($request->hasFile('images')) {
+ 
+     
+         $files = ImageUpload::uploadMultipleImages($request);
+             
+           $data['images'] = json_encode($files);
+        
+         }
+
+       
+     }
+          catch (\Exception $e)
+          { 
+
+           return response()->json([
+              'message'=>$e
+           ]);
+
+          }
+          $Room->update($data);
+
+          return $Room;
+
+        
+   }
+
+ 
+ 
+     public function Delete($room)
+     {
+         $room->files->each(fn ($oldImage) =>
+ 
+            (Storage::exists($oldImage)) ? Storage::delete($oldImage) :  \Log::error("Error deleting file: $room->files"));
+
+            $room->delete();
      }
     }
 
