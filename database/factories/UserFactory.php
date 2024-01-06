e9b0cc66da9dd064da5670bd3dafb1de4c5f33a6
sb ali <125546478+sebbahali@users.noreply.github.com>
2024-01-05 19:16:45 -0800

    Update UserFactory.php

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -30,7 +30,7 @@ class UserFactory extends Factory
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
             'remember_token' => Str::random(10),
-         //   'role_id'=> Role::inRandomOrder()->value('id'),
+           'role_id'=> Role::inRandomOrder()->value('id'),
 
         ];
     }
