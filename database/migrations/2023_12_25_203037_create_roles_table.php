bbee006020277c462e5936f1105a238563b954f4
sb ali <125546478+sebbahali@users.noreply.github.com>
2024-01-05 19:14:06 -0800

    Update 2023_12_25_203037_create_roles_table.php

--- database/migrations/2023_12_25_203037_create_roles_table.php
+++ database/migrations/2023_12_25_203037_create_roles_table.php
@@ -16,6 +16,9 @@ return new class extends Migration
             $table->string('name');
             $table->timestamps();
         });
+          Role::create(['name' => 'User']);
+        Role::create(['name' => 'Admin']);
+        Role::create(['name' => 'Owner']);
     }
 
     /**
