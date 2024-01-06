ec7df9a07a96f8f3e3a2d69607c07cee7bbbeff3
sb ali <125546478+sebbahali@users.noreply.github.com>
2024-01-06 13:27:23 -0800

    Update TestCase.php

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,9 +2,32 @@
 
 namespace Tests;
 
+use App\Models\User;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+
+    public User $user;
+    public User $owner;
+ 
+    protected function setUp() :void
+    {
+
+        parent::setUp();
+
+        $this->user = $this->CreateUser();
+
+      $this->owner =$this->CreateUser(3);
+
+    } 
+      private function CreateUser(int $id=1) :User
+
+{ 
+  
+    return User::factory()->create(['role_id'=>$id]);
+
+}
+
 }
