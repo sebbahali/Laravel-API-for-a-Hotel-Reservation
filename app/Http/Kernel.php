c8fcf5aa91eb91a240ae5142418fddb53b21e2fc
sb ali <125546478+sebbahali@users.noreply.github.com>
2024-01-09 00:43:10 -0800

    Update Kernel.php

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -67,5 +67,7 @@ class Kernel extends HttpKernel
         'signed' => \App\Http\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
+        'role.check' => \App\Http\Middleware\RoleChecking::class,
+
     ];
 }
